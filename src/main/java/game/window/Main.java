42cc55042817b65a34eec45cfcf001b8e32d6c8a
Guy-jirasin <68070015@kmitl.ac.th>
2026-02-13 19:11:35 +0700

    maven initial

--- src/main/java/game/window/Main.java
+++ src/main/java/game/window/Main.java
@@ -1,8 +1,8 @@
 // Dummy file to ensure src directory is tracked
 package game.window;
 
-public class Dummy {
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello from Dummy!");
+        System.out.println("Hello World!");
     }
 }
