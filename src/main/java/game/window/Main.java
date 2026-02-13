e1fe661834501268aceccf290f2f49528daab04c
Guy-jirasin <68070015@kmitl.ac.th>
2026-02-13 19:09:59 +0700

    maven initial commit

---
+++ src/main/java/game/window/Main.java
@@ -0,0 +1,8 @@
+// Dummy file to ensure src directory is tracked
+package game.window;
+
+public class Dummy {
+    public static void main(String[] args) {
+        System.out.println("Hello from Dummy!");
+    }
+}
