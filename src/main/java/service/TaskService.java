31eee2e90e3cdfe87bd44585324c39bd4e919da4
Lico Not A Dev <alternativeclonez@gmail.com>
2026-02-18 09:19:55 +0700

    delete task

--- src/main/java/service/TaskService.java
+++
@@ -1,5 +0,0 @@
-package service;
-//เป็น class ที่ทำงานคล้ายๆ Thread คือการทำงานแบบควบคู่ (Co-Routine) แต่ใช้กับ RunService ได้ ไม่จำเป็นต้องสร้าง Thread เพิ่ม
-//เนื่องจาก Thread เหมาะกับการทำงานที่เยอะๆ มากกว่า ถ้าทำงานเล็กๆ ควรใช้ Co-Routine
-public class TaskService {
-}
