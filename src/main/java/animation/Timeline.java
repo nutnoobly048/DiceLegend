c45a7ceced17cc742f66a16d3b12d395f68a74eb
Panxp <152055816+txe1750@users.noreply.github.com>
2026-03-03 20:20:57 +0700

    update Timeline V2
    add delay in tween + method play(), pause(), stop()

--- src/main/java/animation/Timeline.java
+++ src/main/java/animation/Timeline.java
@@ -1,29 +1,122 @@
 package animation;
 
+import ServiceInterface.ProcessByRunService;
+import service.RunService;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Timeline {
+public class Timeline implements ProcessByRunService {
 
-    private List<Tween> tweenList = new ArrayList<>();
-    private boolean isLooping = false;
-    private double timeScale = 1.0;
+    private static class TimelineItem {
+        Tween tween;
+        double delay;
+        boolean start = false;
 
-    public void add(Tween tween) {
-        tweenList.add(tween);
+        TimelineItem(Tween tween, double delay) {
+            this.tween = tween;
+            this.delay = delay;
+        }
     }
 
-    public void setLooping(boolean bool) {
-        this.isLooping = bool;
+    private List<TimelineItem> tweenList = new ArrayList<>();
+
+    public boolean isLooping = false;
+    public double timeScale = 1.0; // ตัวคูณความเร็ว
+    private boolean isPlaying = false;
+    private double time = 0; // เวลาปจบ --> timeline
+    private double totalDuration = 0;
+
+    public void add(Tween tween, double delay) {
+        TimelineItem item = new TimelineItem(tween, delay);
+        tweenList.add(item);
+        double tweenEnd = delay;
+        totalDuration = Math.max(totalDuration, tweenEnd);
     }
 
-    public void setTimeScale(double scale) {
-        this.timeScale =  scale;
+    public double getTotalDuration() {
+        return totalDuration;
+    }
+
+    // copy tweenList
+    public List<Tween> getTweenList() {
+        List<Tween> list = new ArrayList<>();
+        for (TimelineItem item : tweenList) {
+            list.add(item.tween);
+        }
+        return list;
     }
 
     public void play() {
-        for (Tween t : tweenList) {
-            t.start();
+        if (isPlaying){return;}
+        isPlaying = true;
+        RunService.GetService().addProcess(this);
+    }
+
+    public void pause() {
+        isPlaying = false;
+    }
+
+    public void stop() {
+        isPlaying = false;
+        time = 0;
+        for (TimelineItem item : tweenList) {
+            item.start = false;
+        }
+        RunService.GetService().removeProcess(this);
+    }
+
+    private void restart() {
+        time = 0;
+        for (TimelineItem item : tweenList) {
+            item.start = false;
         }
     }
+
+
+    @Override
+    public void OnCreate() {}
+
+    @Override
+    public void OnUpdate(double deltatime) {
+        if (!isPlaying) {return;}
+
+        time += deltatime * timeScale;
+        for (TimelineItem item : tweenList) {
+            if (!item.start && time >= item.delay) {
+                item.start = true;
+                item.tween.start();
+            }
+        }
+
+        if (time >= totalDuration) {
+            if (isLooping) {restart();}
+            else {stop();}
+        }
+    }
+
+    @Override
+    public void OnLateUpdate() {}
+
+    @Override
+    public void OnRemoved() {}
+
 }
+
+/*
+วิธีใช้ เดี่ยวค่อยลบ
+Timeline timeline = new Timeline();
+\
+สรา้ง Tween
+Tween moveX = new Tween(player, TweenProperty.X, 100, 500, 2);
+Tween moveY = new Tween(player, TweenProperty.Y, 100, 300, 2);
+
+เลื่อน x ก่อนค่อยเลื่อน y
+timeline.add(moveX, 0);
+timeline.add(moveY, 2);
+
+เลื่อนพร้อมกัน
+timeline.add(moveX, 0);
+timeline.add(moveY, 0);
+
+timeline.play();
+*/
