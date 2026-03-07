d1e2582532beda1a0f22898f34ddec1053b39db6
NutNoobly <12345678956sda@gmail.com>
2026-03-07 19:50:03 +0700

    Make Board

--- src/main/java/Gameplay/Cell.java
+++ src/main/java/Gameplay/Cell.java
@@ -2,21 +2,43 @@ package Gameplay;
 
 import ServiceInterface.CellAttribute;
 
+import java.util.ArrayList;
 import java.util.EnumSet;
 
 
 
 public class Cell {
+
+    public Cell(int index) {
+        this(index,0,0, 0);
+    }
+
+    public Cell(int index, int xPos, int yPos) {
+
+        this(index, xPos, yPos, 0);
+
+    }
+
     public Cell(int index, int xPos, int yPos, int destinationIndex) {
         this.index = index;
         this.xPos = xPos;
         this.yPos = yPos;
         this.destinationIndex = destinationIndex;
+
+        if (index == 0) {
+
+            attributes.add(CellAttribute.START_TILE);
+
+        }
+        if (index == 99) {
+            attributes.add(CellAttribute.WIN_TILE);
+        }
     }
 
     public int index;
     public int xPos;
     public int yPos;
+
     public EnumSet<CellAttribute> attributes = EnumSet.noneOf(CellAttribute.class);
 
     public int destinationIndex;
--- src/main/java/objectClass/Board.java
+++ src/main/java/objectClass/Board.java
@@ -1,4 +1,24 @@
 package objectClass;
 
+import Gameplay.Cell;
+import ServiceInterface.CellAttribute;
+
 public class Board {
+
+    private final Cell[] cells = new Cell[100];
+
+    public Board(int[][] positions) {
+        for (int i = 0; i < 100; i++) {
+
+            cells[i] = new Cell(i);
+
+        }
+    }
+
+    public void addAttribute(int index, CellAttribute attribute) {
+
+        cells[index].attributes.add(attribute);
+
+    }
+
 }
