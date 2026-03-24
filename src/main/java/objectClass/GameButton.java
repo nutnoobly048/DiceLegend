4d7eb6e5405f7b58415419bbd7b1ab91a2fec6f4
PMIPX <phedanatesilaarj@gmail.com>
2026-03-24 21:09:23 +0700

    Update GameButton.java

--- src/main/java/objectClass/GameButton.java
+++ src/main/java/objectClass/GameButton.java
@@ -54,18 +54,7 @@ public class GameButton extends JButton {
     }
 
     public GameButton(String text) {
-        super(text);
-        setText("");
-        this.setContentAreaFilled(false);
-        this.setBorderPainted(false);
-        this.setFocusPainted(false);
-        this.setOpaque(false);
-        this.setCursor(new Cursor(Cursor.HAND_CURSOR));
-        this.addMouseListener(new MouseAdapter() {
-            @Override public void mouseReleased(MouseEvent e) { onButtonClicked.run(); }
-            @Override public void mouseEntered(MouseEvent e)  { onMouseEntered.run(); }
-            @Override public void mouseExited(MouseEvent e)   { onMouseExited.run();  }
-        });
+        this(text, "blank.png", "blank.png");
     }
 
     public void setOnButtonClicked(Runnable onButtonClicked) {
