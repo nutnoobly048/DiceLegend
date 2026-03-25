2475e8d2ad6049a9f87ed7987caa0d061f6b1cc3
HRZXD <232311759+ChayakornAree@users.noreply.github.com>
2026-03-25 13:03:47 +0700

    create random event and random item then update checking full room ? or > 4 ?

---
+++ src/main/java/OtherUtilities/RandomItems.java
@@ -0,0 +1,70 @@
+package OtherUtilities;
+
+import java.util.*;
+import Item.*;
+
+public class RandomItems {
+
+    private static HashMap<String, ArrayList<Item>> allItems = new HashMap<>();
+    
+    static {
+
+        ArrayList<Item> defaultItems = createDefaultItems();
+
+        allItems.put("mysteriousJungle", createMysteriousJungleItems(defaultItems));
+        allItems.put("cryoGard", createCryoGardItems(defaultItems));
+        allItems.put("goldenSeason", createGoldenSeasonItems(defaultItems));
+        
+    }
+
+    
+    public static ArrayList<Item> createDefaultItems(){
+
+        ArrayList<Item> list = new ArrayList<>();
+        // เพิ่ม Item ที่มีทุกด่านตรงนี้
+        // list.add(new Item());
+        list.add(new DoubleDiceItem());
+        list.add(new SpineItem());
+        list.add(new SwapItem());
+        return list;
+
+    }
+
+    public static ArrayList<Item> createMysteriousJungleItems(ArrayList<Item> base){
+
+        ArrayList<Item> list = new ArrayList<>(base);
+        // เพิ่ม Item ของด่าน Mysterious Jungle ตรงนี้
+        // list.add(new Item());
+        return list;
+
+    }
+
+    public static ArrayList<Item> createCryoGardItems(ArrayList<Item> base){
+
+        ArrayList<Item> list = new ArrayList<>(base);
+        // เพิ่ม Item ของด่าน Cryo-Gard ตรงนี้
+        // list.add(new Item());
+        list.add(new BorealisItem());
+        return list;
+
+    }
+
+    public static ArrayList<Item> createGoldenSeasonItems(ArrayList<Item> base){
+
+        ArrayList<Item> list = new ArrayList<>(base);
+        // เพิ่ม Item ของด่าน Golden Season ตรงนี้
+        // list.add(new Item());
+        return list;
+
+    }
+
+    // Method สำหรับส่งผลลัพธ์การสุ่ม วิธีใช้ => RandomItems.resultRandomItem();
+    public static Item resultRandomItem(String selectedMap){
+
+        ArrayList<Item> canUseItems = allItems.get(selectedMap);
+        int index = RandomUtilities.randomInt(canUseItems.size());
+        return canUseItems.get(index);
+
+    }
+
+}
